8afac9648af30e7ad1d9435384e5b05f81ab464b
EC2 Default User <ec2-user@ip-172-31-35-124.us-east-2.compute.internal>
2019-06-30 20:54:24 +0000

    raw birthstone app to try to deploy to heroku

---
+++ app/models/model.py
@@ -0,0 +1,32 @@
+def get_birthstone(date):
+    year, month, day = date.split('-')
+    
+    month = int(month)
+    day = int(day) 
+
+    if month == 12:
+        stone = 'topaz' if (day < 22) else 'ruby'
+    elif month == 1:
+        stone = 'ruby' if (day < 20) else 'garnet'
+    elif month == 2:
+        stone = 'garnet' if (day < 19) else 'amethyst'
+    elif month == 3:
+        stone = 'amethyst' if (day < 21) else 'bloodstone'
+    elif month == 4:
+        stone = 'bloodstone' if (day < 20) else 'sapphire'
+    elif month == 5:
+        stone = 'sapphire' if (day < 21) else 'agate'
+    elif month == 6:
+        stone = 'agate' if (day < 21) else 'emerald'
+    elif month == 7:
+        stone = 'emerald' if (day < 23) else 'onyx'
+    elif month == 8:
+        stone = 'onyx' if (day < 23) else 'carnelian'
+    elif month == 9:
+        stone = 'carnelian' if (day < 23) else 'chrysolite'
+    elif month == 10:
+        stone = 'chrysolite' if (day < 23) else 'beryl'
+    elif month == 11:
+        stone = 'beryl' if (day < 22) else 'topaz'
+  
+    return stone
---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template, request
+from app.models import model
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/birthstone', methods = ['GET', 'POST'])
+def birthstone():
+    if request.method == "GET":
+        return "<h1> use the form on the <a href='/'> home page </a> </h1>"
+
+    data = dict(request.form)
+
+    birthstone = model.get_birthstone(data[u'date'][0])
+
+    return render_template("birthstone.html", birthstone=birthstone)
